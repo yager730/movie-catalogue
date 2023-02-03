f4eab1ac7f845886a4ddee9e585e727b86e3d341
Thomas Yager <tyager@singlestoneconsulting.com>
2023-02-03 16:53:40 -0600

    Style for reviews list working, needs pagination and sort... Reviews are ordered by watch date... need to style individual reviews page with scrollable log

--- src/app/reviews-list/reviews-list.component.ts
+++ src/app/reviews-list/reviews-list.component.ts
@@ -4,15 +4,29 @@ import { map, Subscription } from 'rxjs';
 import { movieReview, movieReviews } from './review.model';
 import * as fromApp from '../store/app.reducer';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as Utils from '../shared/utils';
+import { MovieDetails } from '../shared/movie-info.model';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-reviews-list',
   templateUrl: './reviews-list.component.html',
-  styleUrls: ['./reviews-list.component.css']
+  styleUrls: ['./reviews-list.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class ReviewsListComponent implements OnInit, OnDestroy {
   reviewsSubscription: Subscription;  
   reviewsList: movieReviews[];
+  tableDisplayCols = ['title', 'lastWatched', 'timesLogged', 'rating', 'options', 'expand'];
+
+  columnsToDisplayWithExpand = [...this.tableDisplayCols, 'expand'];
+  expandedElement: movieReviews | null;
 
   constructor ( private store: Store<fromApp.AppState>, 
     private router: Router, private route: ActivatedRoute ) {}
@@ -26,9 +40,13 @@ export class ReviewsListComponent implements OnInit, OnDestroy {
     })
   }
 
-  ishClicked(film: movieReviews) {
+  goToReviews(film: movieReviews) {
     this.router.navigate([`./id/${film.movieDetails.id}`], { relativeTo: this.route });
   }
+
+  getReleaseDate(film: MovieDetails) { return Utils.getReleaseDate({ movieDetails: film, movieCrew: null, movieImagePaths: null }) };
+  getRating(film: MovieDetails) { return Utils.getRating({ movieDetails: film, movieCrew: null, movieImagePaths: null }) };
+
 
   ngOnDestroy() {
     this.reviewsSubscription.unsubscribe();
--- src/app/reviews-list/store/reviews.reducer.ts
+++ src/app/reviews-list/store/reviews.reducer.ts
@@ -11,6 +11,13 @@ const initState: State = {
     reviewsList: []
 };
 
+// Want to sort reviews by watchDate order, more recent the review the earlier it comes in the reviews array
+function sortByDate(data: movieReview[]): movieReview[] {
+    return data.sort((a, b) => {
+        return (a.watchDate < b.watchDate ? 1 : -1);
+    });
+}
+
 export function reviewsReducer(state: State = initState, action: ReviewActions.ReviewActions) {
     let updatedReviewList: movieReviews[];
     let updateIndex: number;
@@ -22,7 +29,7 @@ export function reviewsReducer(state: State = initState, action: ReviewActions.R
             if (state.reviewsList.map(review => review.movieDetails.id).includes(action.payload.movieInfo.movieDetails.id)) {
                 updatedReviewList = [...state.reviewsList];
                 updateIndex = state.reviewsList.findIndex((el) => el.movieDetails.id === action.payload.movieInfo.movieDetails.id);
-                updatedReviews = [...state.reviewsList[updateIndex].reviews, action.payload.review];
+                updatedReviews = sortByDate([...state.reviewsList[updateIndex].reviews, action.payload.review]);
                 updatedMovieReviews = {
                     movieDetails: action.payload.movieInfo.movieDetails, 
                     reviews: updatedReviews
@@ -41,7 +48,7 @@ export function reviewsReducer(state: State = initState, action: ReviewActions.R
             updatedReviewList = [...state.reviewsList];
             updateIndex = state.reviewsList.findIndex((el) => el.movieDetails.id === action.payload.movieInfo.movieDetails.id);
             const updatedReview = action.payload.review;
-            updatedReviews = updatedReviewList[updateIndex].reviews.map((el, i) => i === action.payload.index ? updatedReview : el);
+            updatedReviews = sortByDate(updatedReviewList[updateIndex].reviews.map((el, i) => i === action.payload.index ? updatedReview : el));
             updatedMovieReviews = { 
                 movieDetails: action.payload.movieInfo.movieDetails,
                 reviews: updatedReviews
